0e09100e63dcb8ea9780d82ef37a58ba2815fbdb
Yosa Yon <yosa.yon@gmail.com>
2018-01-25 17:50:52 +0000

    added two class methods to start the program and to create a list of companies

--- lib/best_companies/cli.rb
+++ lib/best_companies/cli.rb
@@ -1,6 +1,13 @@
 class BestCompanies::CLI
-    BASE_PATH = "https://www.greatplacetowork.com/best-workplaces/100-best/2017"
+ BASE_PATH = "https://www.greatplacetowork.com/best-workplaces/100-best/2017"
     
-    def start
-    end
+ def self.start
+  create_list
+  
+ end
+ 
+ def self.create_list
+  company_hash = BestCompanies::Scraper.new.scrape_companies(BASE_PATH)
+  BestCompanies::Company.create_from_list(company_hash)
+ end
 end
