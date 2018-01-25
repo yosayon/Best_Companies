c0d1374ed7d0027355d3f91f03421f243a3db5c7
Yosa Yon <yosa.yon@gmail.com>
2018-01-25 04:23:01 +0000

    added initialize methods and custom constructors and add_ratings and add_awards instance methods

--- lib/best_companies/company.rb
+++ lib/best_companies/company.rb
@@ -2,7 +2,22 @@ class BestCompanies::Company
     attr_accessor :name, :industry, :location, :review_url, :challenges, :atmosphere, :rewards, :pride, :communication, :bosses, :awards
     @@all =[]
     
+    def initialize(company_hash)
+        company_hash.each{|key,value|self.send("#{key}=", value)}
+        @@all << self
+    end
         
+    def self.create_from_list(company_array)
+    company_array.each{|company|self.new(company)}
+   end
+   
+   def add_ratings(ratings_hash)
+    ratings_hash.each{|key,value|self.send("#{key}=",value)}
+  end
+  
+  def add_awards(awards_hash)
+    awards_hash.each{|key,value|self.send("#{key}=",value)}
+  end
     
     
 end
