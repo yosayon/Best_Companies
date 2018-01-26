e2aa7fdc12be73f1f977ffba6e80a57f37e06c24
Yosa Yon <yosa.yon@gmail.com>
2018-01-26 21:06:56 +0000

    added an option for user to input custom ranges to view the top 100 list

--- lib/best_companies/cli.rb
+++ lib/best_companies/cli.rb
@@ -3,40 +3,59 @@ class BestCompanies::CLI
     
  def self.start
   create_list
-  puts "Welcome!"
-  puts "To see the top 5 Best Companies please type 1-5"
-  puts "To see the top 10 Best Companies please type 1-10"
-  puts "To see the top 20 Best Companies please type 1-20"
-  puts "To see the top 50 Best Companies please type 1-50"
-  puts "To see the entire list of Best Companies please type 'see list'."
-  puts "To enter in a custom range between 1-100, please include a dash. For example, '15-20'."
-  input = gets.strip.to_s
+  input = ""
+   puts "To see the top 5 Best Companies please type 1-5"
+   puts "To see the top 10 Best Companies please type 1-10"
+   puts "To see the top 20 Best Companies please type 1-20"
+   puts "To see the top 50 Best Companies please type 1-50"
+   puts "To see the entire list of Best Companies please type 'see list'"
+   puts "To enter in a custom range, type 'custom'"
+   puts "To exit type 'exit"
+   
+   input = gets.strip.to_s
   
   case input
   when "1-5"
-   self.see_list(5)
+   self.see_list(0,4)
   when "1-10"
-   self.see_list(10)
+   self.see_list(0,9)
   when "1-20"
-   self.see_list(20)
+   self.see_list(0,19)
   when "1-50"
-   self.see_list(50)
+   self.see_list(0,49)
   when "see list"
-   self.see_list(100)
-   else
-    puts "That's not a valid input:"
+   self.see_list(0,99)
+  when "custom"
+   self.custom_list
+  when "exit"
+   exit
+  else
+   puts "That's not a valid input:"
+   self.start
+  end
+  
+  while input != "exit"
     self.start
   end
  end
  
- def self.see_list(range)
-  BestCompanies::Company.all.slice(0, range.to_i).each do |company|
+ def self.see_list(num1, num2)
+  BestCompanies::Company.all.slice(num1..num2).each do |company|
    puts "Name: #{company.name}"
    puts "Industry: #{company.industry}"
    puts "Location: #{company.location}"
    puts "Review_URL: #{company.review_url}"
    puts "------------------------------"
    end
+ end
+
+ def self.custom_list
+  puts "Please enter your range between 1-100, separated by a dash. For example: 15-20"
+  input = gets.strip.split("-")
+  
+  num1 = (input[0].to_i)-1
+  num2 = (input[1].to_i)-1
+  self.see_list(num1,num2)
  end
  
  def self.create_list
