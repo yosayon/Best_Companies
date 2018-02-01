4f193de6070a1d48ac4ece32a8157ca6376e1621
Yosa Yon <yosa.yon@gmail.com>
2018-02-01 01:15:51 +0000

    separated enter_state and validate_input into a separate method

--- lib/best_companies/state.rb
+++ lib/best_companies/state.rb
@@ -9,19 +9,19 @@ class BestCompanies::State
  end  
 
  def add_company(company)
-   @company << company.name unless @company.include?(company.name)
+  @company << company.name unless @company.include?(company.name)
  end
  
  def self.create(name)
- self.new(name).tap{|i|i.save}
+  self.new(name).tap{|i|i.save}
  end
  
  def save
- self.class.all << self
+  self.class.all << self
  end
  
  def self.find(name)
- all.detect{|i|i.name == name}
+  all.detect{|i|i.name == name}
  end
  
  def self.find_or_create_by_name(name)
@@ -34,6 +34,7 @@ class BestCompanies::State
  end
  
  def self.list_all_states
+  puts "-------------------------------"
   states = self.all.sort{|a,b| a.name <=> b.name}
   states.each{|s|puts "#{s.name}"}
   self.enter_state
@@ -45,22 +46,31 @@ class BestCompanies::State
   puts "Please enter the state to view the list of companies"
   puts "To see the list of states again type 'see states'"
   puts "Type menu, to go back to the main menu"
+  puts "------------------------------------------"
   input = gets.strip
-  
-  validate_input = self.all.detect{|state|state.name == input}
-  
-  if input == "menu"
-   BestCompanies::CLI.ask_user
-  elsif input == "see states"
-   self.list_all_states
-  elsif validate_input.name != nil
-    puts "#{validate_input.company}"
-    self.enter_state
+  case input
+   when "menu"
+    BestCompanies::CLI.ask_user
+   when "see states"
+    self.list_all_states
+   when "exit"
+    exit
+   else
+    self.validate_input(input)
+  end
+ end
+   
+ def self.validate_input(input)
+  validated_input = self.all.detect{|state|state.name == input}
+  if validated_input != nil
+   validated_input.company.each{|company|puts "#{company}"}
+   self.enter_state
   else
-   puts "That's not a valid input. Please try again."
-   self.list_all_states
+   puts "invalid input, please try again."
+   self.enter_state
   end
  end
  
 end
+ 
  
