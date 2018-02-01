163ba2f65cad1db6e1280704b1d1fff5eaf93dcb
Yosa Yon <yosa.yon@gmail.com>
2018-02-01 23:54:40 +0000

    added class methods find_by_name and find_or_create_by_name

--- lib/best_companies/concerns/findable.rb
+++ lib/best_companies/concerns/findable.rb
@@ -1,8 +1,14 @@
 module Concerns::Findable
-    def find_by_name(name)
-    end
     
-    def find_or_create_by_name(name)
-    end
-    
+ module ClassMethods
+  def find_by_name(name)
+   all.detect{|i|i.name == name}
+  end
+  
+  def find_or_create_by_name(name)
+   name = name.split(", ")[1]
+  find(name) || create(name)
+  end
+ end 
+ 
 end
