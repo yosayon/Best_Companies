931b813a3e85523cc7e878571d9768d239d22770
Yosa Yon <yosa.yon@gmail.com>
2018-02-02 22:04:36 +0000

    add and removed binding.pry

--- lib/best_companies/cli.rb
+++ lib/best_companies/cli.rb
@@ -103,7 +103,8 @@ class BestCompanies::CLI
   puts "Pride: #{company.pride}"
   puts "Communication: #{company.communication}"
   puts "Bosses: #{company.bosses}"
-  puts "Awards: #{company.awards}"
+  puts "Awards: \n"
+  company.awards.each{|award|puts "#{award}"}
   puts "------------------------------------------------"
  end
 
