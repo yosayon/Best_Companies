0ce0972fa048492bce09a507ddc14500fff5083b
Yosa Yon <yosa.yon@gmail.com>
2018-02-02 00:02:05 +0000

    removed all findable, persistable, and nameable methods

--- lib/best_companies/state.rb
+++ lib/best_companies/state.rb
@@ -12,23 +12,6 @@ class BestCompanies::State
   @company << company.name unless @company.include?(company.name)
  end
  
- def self.create(name)
-  self.new(name).tap{|i|i.save}
- end
- 
- def save
-  self.class.all << self
- end
- 
- def self.find(name)
-  all.detect{|i|i.name == name}
- end
- 
- def self.find_or_create_by_name(name)
-  state = name.split(", ")[1]
-  find(state) || create(state)
- end
- 
  def self.all
   @@all
  end
