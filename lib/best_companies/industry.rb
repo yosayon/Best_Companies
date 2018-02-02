6de7a5e741fa7cac1e37042304264f0788032970
Yosa Yon <yosa.yon@gmail.com>
2018-02-02 00:08:12 +0000

    removed persistable, nameable, and findable methods from industry class

--- lib/best_companies/industry.rb
+++ lib/best_companies/industry.rb
@@ -11,22 +11,6 @@ class BestCompanies::Industry
   @company << company.name unless @company.include?(company.name)
  end
   
- def self.create(name)
-   self.new(name).tap{|i|i.save}
- end
- 
- def save
-  self.class.all << self
- end
- 
- def self.find(name)
-  all.detect{|i|i.name == name}
- end
-  
- def self.find_or_create_by_name(name)
-  find(name) || create(name)
- end
-  
  def self.all
   @@all
  end
