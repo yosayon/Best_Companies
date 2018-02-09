f45f46ea89e01d7f2aa49a2573a71847a575e6c2
Yosa Yon <yosa.yon@gmail.com>
2018-02-09 23:34:23 +0000

    implementing DRY

--- lib/best_companies/cli.rb
+++ lib/best_companies/cli.rb
@@ -15,56 +15,95 @@ class BestCompanies::CLI
  def self.ask_user
   input = ""
   puts "\n------------------------------------------------"
-  puts "To see the top 10 Best Companies please type 1-10".colorize(:light_blue)
-  puts "To see the top 20 Best Companies please type 1-20".colorize(:light_blue)
-  puts "To see the top 50 Best Companies please type 1-50".colorize(:light_blue)
+  #puts "To see the top 10 Best Companies please type 1-10".colorize(:light_blue)
+  #puts "To see the top 20 Best Companies please type 1-20".colorize(:light_blue)
+  #puts "To see the top 50 Best Companies please type 1-50".colorize(:light_blue)
   puts "To see the entire list of Best Companies please type 'see list'".colorize(:light_blue)
-  puts "To enter in a custom range, type 'custom'".colorize(:light_blue)
+  puts "To enter in a custom range of Best Companies between 1-100, type the range in number-number format For Ex: 15-20".colorize(:light_blue)
+  puts "To view the ratings and awards for a company, enter the company rank (1-100). For Ex: 5".colorize(:light_blue)
   puts "To view best companies by state, type 'state'".colorize(:light_blue)
-  puts "To exit type 'exit".colorize(:light_blue)
+  puts "Type archive to see your saved companies".colorize(:light_blue)
+  puts "To exit type exit".colorize(:light_blue)
   puts "------------------------------------------------"
   input = gets.strip.to_s
   
+  self.validate_input(input)
+  
   case input
-  when "1-10"
-   BestCompanies::Company.list_all(0,9)
-   self.see_ratings_and_awards
-  when "1-20"
-   BestCompanies::Company.list_all(0,19)
-   self.see_ratings_and_awards
-  when "1-50"
-   BestCompanies::Company.list_all(0,49)
-   self.see_ratings_and_awards
+  #when "1-10"
+  # BestCompanies::Company.list_all(0,9)
+  # self.see_ratings_and_awards
+  #when "1-20"
+  # BestCompanies::Company.list_all(0,19)
+  # self.see_ratings_and_awards
+  #when "1-50"
+  # BestCompanies::Company.list_all(0,49)
+   #self.see_ratings_and_awards
   when "see list"
    BestCompanies::Company.list_all(0,99)
-   self.see_ratings_and_awards
-  when "custom"
-   self.custom_list
-   self.see_ratings_and_awards
+   #self.see_ratings_and_awards
+  #when "custom"
+  # self.custom_list
+  # self.see_ratings_and_awards
   when "state"
    BestCompanies::State.list_all_states
+  when "archive"
+   BestCompanies::Company.archive
   when "exit"
    exit
   else
-   puts "That's not a valid input:"
-   self.ask_user
+   if input.match(/\d{1,}\-\d{1}/)
+    input = input.split("-")
+    num1 = (input[0].to_i)-1
+    num2 = (input[1].to-i)-1
+    BestCompanies::Company.list_all(num1,num2)
+    #self.see_ratings_and_awards
+   elsif input.match(/\d{1,}/) && input.to_i.between?(1,100)
+     
+  # puts "That's not a valid input:"
+  # self.ask_user
+   else
+    puts "That's an invalid input. Please try again."
+   end
   end
   
   while input != "exit"
     self.ask_user
   end
-  
+ end
+ 
+ def self.validate_input(input)
+  if input.match(/\d{1,}\-\d{1}/)
+    input = input.split("-")
+    num1 = (input[0].to_i)-1
+    num2 = (input[1].to-i)-1
+    if num1 < num2 && num2.between?(1,100)
+     BestCompanies::Company.list_all(num1,num2)
+    else
+     self.reject_input
+    end
+   elsif input.match(/\d{1,}/) && input.to_i.between?(1,100)
+    self.add_ratings_and_awards
+   else
+    self.reject_input
+  end
+ end
+ 
+ def self.reject_input
+  puts "Your input sucks and you should input something else."
+  self.ask_user
  end
  
  def self.see_ratings_and_awards
-  puts "To view the ratings and awards for a company, enter the company rank".colorize(:light_blue)
-  puts "To enter in a custom range, type 'custom'".colorize(:light_blue)
+  #puts "To view the ratings and awards for a company, enter the company rank".colorize(:light_blue)
+  #puts "To enter in a custom range, type 'custom'".colorize(:light_blue)
   puts "Type menu to go back to the main menu".colorize(:light_blue)
   puts "Type archive to see your saved companies".colorize(:light_blue)
   puts "Type exit to exit".colorize(:light_blue)
   puts "------------------------------------------------"
   
   input = gets.strip
+  
   
   case input
   when "menu"
@@ -124,21 +163,21 @@ class BestCompanies::CLI
   end
  end
 
- def self.custom_list
-  puts "------------------------------------------------"
-  puts "Please enter your range between 1-100, separated by a dash. For example: 15-20".colorize(:light_blue)
-  puts "------------------------------------------------"
-  input = gets.strip
-  if input.match(/\d{1,}\-\d{1}/)
-   input = input.split("-")
-   num1 = (input[0].to_i)-1
-   num2 = (input[1].to_i)-1
-   BestCompanies::Company.list_all(num1,num2)
-  else
-   puts "------------------------------------------------"
-   puts "That's an invalid range"
-   self.custom_list
-  end
+ #def self.custom_list
+  #puts "------------------------------------------------"
+  #puts "Please enter your range between 1-100, separated by a dash. For example: 15-20".colorize(:light_blue)
+  #puts "------------------------------------------------"
+  #input = gets.strip
+  #if input.match(/\d{1,}\-\d{1}/)
+  # input = input.split("-")
+  # num1 = (input[0].to_i)-1
+  # num2 = (input[1].to_i)-1
+  # BestCompanies::Company.list_all(num1,num2)
+  #else
+  # puts "------------------------------------------------"
+  # puts "That's an invalid range"
+  # self.custom_list
+  #end
  end
  
 end
